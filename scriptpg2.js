632c66c2058fe9e70d9a940d8a7438fc3d9332b1
Nicholas-py <74473358+Nicholas-py@users.noreply.github.com>
2023-10-22 18:28:39 -0400

    Make selections earlier

--- scriptpg2.js
+++ scriptpg2.js
@@ -49,7 +49,6 @@ function topbuttonclicked(button) {
 
 function bottombuttonclicked(button) {
   console.log(button);
-  topbuttonselection.disabled = false;
   var topbuttonname = topbuttonselection.innerHTML;
   var bottombuttonname = button.innerHTML;
   var topbuttonindex = names.indexOf(topbuttonname);
@@ -62,9 +61,9 @@ function bottombuttonclicked(button) {
 
   hiddentext.innerHTML = topbuttonname+" can't get "+bottombuttonname;
   show(hiddentext);
+
+  
 }
-
-
 
 
 function pg2load() {
@@ -98,4 +97,78 @@ function pg2load() {
 
 function nextpage() {
   localStorage.setItem('bigarray',stringify(bigarray));
+  var selections = makeselections(bigarray)
+  localStorage.setItem('selections',stringify(selections))
+}
+
+
+
+function randint(a,b) {
+  var randseed = Math.random();
+  randseed *= b-a;
+  randseed = Math.floor(randseed);
+  return randseed + a;
+}
+
+function listfullydefined(list) {
+  var b = true;
+  for (let i=0; i<list.length; i++) {
+    if (typeof list[i] == 'undefined') {
+      b = false;
+    }
+  }
+  return b;
+}
+
+
+function randomelement(list) {
+  var randindex = randint(0,list.length);
+  return list[randindex];
+}
+
+function copy(list) {
+  return JSON.parse(JSON.stringify(list));
+}
+
+function deleteaname(namessquare,name, starti) {
+  for (let i = starti; i < namessquare.length; i++) {
+    let indexplace = namessquare[i].indexOf(name);
+    if (indexplace !== -1) {
+      namessquare[i].splice(indexplace,1);
+      if (namessquare[i].length == 0) {
+        console.log('returned false');
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function attemptselections(namessquare) {
+  console.log('attempting');
+  var items = [];
+  var b = true;
+  var worked = true;
+  for (let i = 0; i < namessquare.length; i++) {
+    let item = randomelement(namessquare[i]);
+    worked = deleteaname(namessquare,item,i+1);
+    if (!worked) {
+      return false;
+    }
+
+    items.push(item);
+
+  }  
+  return items;
+}
+
+function makeselections (array) {
+  var blerp = false;
+  var i = 0;
+  while (!blerp && i < 100){
+    blerp = attemptselections(copy(array));
+    console.log(blerp);
+    i++
+  }
+  return blerp;
 }
